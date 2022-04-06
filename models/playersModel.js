4e2f91a6500d675f2c8dfd5a48e660d201e5344b
Dafidaker <tiago7carapinha@gmail.com>
2022-04-06 12:30:25 +0100

    trying

--- models/playersModel.js
+++ models/playersModel.js
@@ -4,11 +4,11 @@ module.exports.loginCheck = async function (name,password) {
     try {
       let sql = `Select player_id from player where player_name = $1 and player_password = $2`;
       let result = await pool.query(sql,[name,password]);
-      console.log( '%s', result.rows.length);
       if (result.rows.length == 0) {
           return { status: 401, result: {msg: "Wrong password or username."}}
       }
       let ply_id = result.rows[0].player_id;
+      console.log(ply_id);
       return { status: 200, result: {msg: "Login correct", userId : ply_id} };
     } catch (err) {
       console.log(err);
