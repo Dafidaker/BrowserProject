c174537930b2db35746bedfc2ea8bbdc52d69c56
Raul Schneider <raulschneider@hotmail.com>
2022-04-07 08:47:30 +0100

    testing

--- models/connection.js
+++ models/connection.js
@@ -4,11 +4,12 @@ const connectionString = process.env.DATABASE_URL
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
-    max: 10,
+    max: 10
+    /*,
       ssl: {
           require: true, 
           rejectUnauthorized: false
-      }
+      }*/
 })
 
 module.exports = pool;
