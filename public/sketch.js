6111a6815bb0b6077c3f59638b27e09b90495f42
Dafidaker <tiago7carapinha@gmail.com>
2022-04-07 09:16:04 +0100

    aa

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,25 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1920, 1080);
   }
   
+  var i = 100
+
   function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
+    /* draw_all_rect() */
+    do {
+        console.log(a)
+        rect(350 + i , 220, 100, 100);
+        i=i + 100;
+      }
+      while (i < 1000); 
   }
+
+/* function draw_rect(i){
+    rect(350 + i , 220, 100, 100);
+}
+
+function draw_all_rect(){ 
+    for (var i = 100 ; i < 1000 ; i = i +100){
+        draw_rect(i)
+          }  
+} */
